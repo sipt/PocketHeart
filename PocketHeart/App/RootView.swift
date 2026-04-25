b95ec371e294c10849d30b5410aeb50cb4a19e79
sipt <wxysipt@gmail.com>
2026-04-26 02:58:30 +0800

    chore: remove template scaffold, add mic+speech usage strings

---
+++ PocketHeart/App/RootView.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Voice Ledger")
+            .foregroundStyle(.white)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.black)
+    }
+}
